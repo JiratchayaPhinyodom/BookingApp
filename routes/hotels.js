068c5767295faac8d69375cfac4f6c0860cf76f6
jiratchayaphinyodom <jiratchaya.phiny@ku.th>
2023-01-21 17:12:03 +0700

    Create database by using mongoDB

---
+++ routes/hotels.js
@@ -0,0 +1,79 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+
+//Create
+router.post("/", async (req,res)=>{
+    
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//Update
+router.put("/:id", async (req,res)=>{
+
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, 
+            { $set: req.body},
+            { new: true }
+            )
+        res.status(200).json(updatedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//Delete
+router.delete("/:id", async (req,res)=>{
+
+    try{
+        await Hotel.findByIdAndDelete(
+            req.params.id, 
+            );
+        res.status(200).json("Hotel has been deleted.")
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Get
+router.get("/:id", async (req,res)=>{
+
+    try{
+        const hotel = await Hotel.findById(
+            req.params.id, 
+            );
+        res.status(200).json(hotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Get All
+router.get("/", async (req,res, next)=>{ 
+
+    // console.log("Hi im a hotel route")
+    try{
+        const hotels = await Hotel.findById(
+            "asasasad"
+            );
+        res.status(200).json(hotels)
+
+    }catch(err){
+        next(err)
+    }
+})
+
+
+export default router
